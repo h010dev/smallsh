3e5e78d69fbceb5f252f281d7a7844a8950d588e
Mohamed Al-Hussein <mohamed.n.al.hussein@gmail.com>
2022-02-07 18:02:15 -0500

    Complete core functions for signal handler module
    Also add extra docs and error handling

--- include/signals/handler.h
+++ include/signals/handler.h
@@ -1,10 +1,27 @@
-//
-// Created by mohamed on 2/4/22.
-//
-
+/**
+ * @file handler.h
+ * @author Mohamed Al-Hussein
+ * @date 04 Feb 2022
+ * @brief Contains functions for handling signals.
+ */
 #ifndef SMALLSH_HANDLER_H
 #define SMALLSH_HANDLER_H
 
-int file_global_var;
+/**
+ * @brief Handles a SIGCHLD signal.
+ * @param sig signal number
+ */
+void handler_handle_sigchld(int sig);
+
+/**
+ * @brief Handles a SIGTSTP signal.
+ * @param sig signal number
+ */
+void handler_handle_sigtstp(int sig);
+
+// 34.6.1
+void dispatcher_dispatch_sighup(pid_t pid);
+void handler_handle_sighup(int sig);
+void handler_handle_sigint(int sig);
 
 #endif //SMALLSH_HANDLER_H
--- src/signals/handler.c
+++ src/signals/handler.c
@@ -1,41 +1,90 @@
 /**
+ * @file handler.c
+ * @author Mohamed Al-Hussein
  * @date 04 Feb 2022
- * @param sig
+ * @brief Contains functions for handling signals.
  */
-
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
+#include <unistd.h>
 
 #include "signals/handler.h"
+#include "core/dtos.h"
+#include "core/shell-attrs.h"
 
-// 34.6.1
-void dispatcher_dispatch_sighup(pid_t pid);
-void handler_handle_sighup(int sig);
+void handler_handle_sigtstp(int sig)
+{
+        (void) sig;
 
-void handler_handle_sigint(int sig);
-void handler_handle_sigtstp(int sig);
+        int saved_errno;
 
-// tlpi 26.3
+        /* save errno */
+        saved_errno = errno;
+
+#ifdef DEBUG
+        write(STDOUT_FILENO, "sigtstp received\n", 17);
+#endif
+
+        /*
+         * Write a single byte to pipe to notify listener that SIGTSTP was
+         * caught.
+         */
+        errno = 0;
+        if (write(pfd2[1], "x", 1) == -1 && errno != EAGAIN) {
+                perror("write");
+                _exit(1);
+        }
+
+        /* reset errno */
+        errno = saved_errno;
+}
+
 void handler_handle_sigchld(int sig)
 {
         (void) sig;
-        file_global_var = 1;
 
         int status, saved_errno;
         pid_t child_pid;
+        SigchldDTO dto;
 
+        /* save errno */
         saved_errno = errno;
 
+        /*
+         * Catch all incoming SIGCHLD signals and dispatch their status to
+         * listening parties.
+         *
+         * Source: TLPI section 26.3
+         */
+        errno = 0;
         while ((child_pid = waitpid(-1, &status, WNOHANG)) > 0) {
-                // check wait status value (26.1.3)
-                // raise signal to usr type sig in smallsh ?
-                continue;
-        }
+                /*
+                 * Initialize DTO object to transfer PID and status through.
+                 */
+                dto.chld_pid = child_pid;
+                dto.chld_status = status;
 
+#ifdef DEBUG
+                write(STDERR_FILENO, "sigchld received\n", 18);
+#endif
+
+                /*
+                 * Write DTO to pipe.
+                 */
+                errno = 0;
+                if (write(pfd[1], &dto, sizeof(dto)) == -1 && errno != EAGAIN) {
+                        perror("write");
+                        _exit(1);
+                }
+        }
         if (child_pid == -1 && errno != ECHILD) {
-                // error
+                perror("waitpid");
+                _exit(1);
         }
 
+        /* reset errno */
         errno = saved_errno;
 }
