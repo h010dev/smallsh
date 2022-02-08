286c7880ff8465404495cb6f0255940849509001
Mohamed Al-Hussein <mohamed.n.al.hussein@gmail.com>
2022-02-08 01:51:34 -0500

    Complete status builtin

--- src/builtins/status.c
+++ src/builtins/status.c
@@ -1,20 +1,11 @@
 //
 // Created by mohamed on 2/3/22.
 //
+#include <stdio.h>
 
 //https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_08_02
-void status(int status)
+int status(int status)
 {
-        switch (status) {
-                case 127:
-                        // command not found
-                        break;
-                case 126:
-                        // command not executable
-                        break;
-                default:
-                        break;
-        }
+        fprintf(stdout, "exit value %d\n", status);
+        return 0;
 }
-
-// global file status, set_status, get_status ?
