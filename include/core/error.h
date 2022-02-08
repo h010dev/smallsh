3749addb62789de3018f7fdc398de1ee66d7f66f
Mohamed Al-Hussein <mohamed.n.al.hussein@gmail.com>
2022-02-08 01:48:17 -0500

    Create errors module for error handling and reporting

---
+++ include/core/error.h
@@ -0,0 +1,14 @@
+//
+// Created by mohamed on 2/7/22.
+//
+
+#ifndef SMALLSH_ERROR_H
+#define SMALLSH_ERROR_H
+
+#define SMSH_EFATAL -1
+#define SMSH_EOK 0
+#define SMSH_EOPEN 1
+
+volatile int smallsh_errno;
+
+#endif //SMALLSH_ERROR_H
